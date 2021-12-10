26dd191f520ee99702df9e20b6c30f3b62193e3a
Romeo tohoun <romeotohoun@gmail.com>
2021-12-10 12:11:33 +0000

    Modification de votre classe parente:  - on ne peut pas avoir un objet mamifere qui n'apprtient pas à une categorie  - ainsi il faut qu'on rend la classe parente Mamifere abstraite afin d'empecher de creer des objes tde cette classe

--- mamifre.py
+++ mamifre.py
@@ -1,10 +1,22 @@
+from abc import ABC, abstractmethod
 class Mamifere:
     def __init__(self,locomotion,milieu_de_vie) -> None:
         self.locomotion=locomotion
         self.milieu_de_vie=milieu_de_vie
+    
+    @abstractmethod
+    def habitat(self):
+        pass
         
 
 class Humain(Mamifere):
-    pass
+    def habitat(self):
+        print(f"j'habite dans une maison")
+
 class Lemurien(Mamifere):
-    pass
+    def habitat(self):
+        print(f"j'habite dans un terrier")
+
+
+p1 = Humain("pieds","omnivor")
+p1.habitat()
