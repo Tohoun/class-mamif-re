baa20f612381c331fbbf0abf3d26c08f0a592c7f
kidamb4 <ballekidam@gmail.com>
2021-12-10 19:15:58 +0100

    AJOUT DE LA METHODE MDC

--- mamifre.py
+++ mamifre.py
@@ -3,27 +3,31 @@ class Mamifere:
     def __init__(self,locomotion,type) -> None:
         self.locomotion=locomotion
         self.type=type
-    
+        
     @abstractmethod
     def habitat(self):
+        pass
+    @abstractmethod
+    def mdc(self):
         pass
         
 
 class Humain(Mamifere):
     def habitat(self):
-        print(f"j'habite dans une maison")
+        print("Habite dans une maison")
+    def mdc(self):
+        print("Parle")
+
 
 class Lemurien(Mamifere):
     def habitat(self):
-        print(f"j'habite dans un terrier")
+        print("Vit dans un terrier")
+    def mdc(self):
+        print("Fait un bruit de lémurien")
 
-class Caractere(Mamifere):
-    def __init__(self, locomotion, type,mdc) -> None:
-        super().__init__(locomotion, type)
-        self.__mdc=mdc
-# mdc=mode de communication
 #type=("herbivore","carnivore","omnivore")
 
 
 p1 = Humain("pieds","omnivor")
 p1.habitat()
+p1.mdc()
