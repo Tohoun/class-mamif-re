d3e82bc416bc86a4c99b3385ba7d87dc65937709
jean bosco yao <jeanboscoyao1@gmail.com>
2021-12-10 10:54:52 +0000

    première version du programme mamifère

---
+++ mamifre.py
@@ -0,0 +1,10 @@
+class Mamifere:
+    def __init__(self,locomotion,milieu_de_vie) -> None:
+        self.locomotion=locomotion
+        self.milieu_de_vie=milieu_de_vie
+        
+
+class Humain(Mamifere):
+    pass
+class Lemurien(Mamifere):
+    pass
